cf1887f8e1dcb013ed4bd2c131b0dca7759657d1
Shaunak Chandra <79399986+Kingsky1t@users.noreply.github.com>
2025-03-15 14:53:40 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -142,5 +142,5 @@ app.post("/update-customer-metafields", async (req, res) => {
 });
 
 
-
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log("Server running on port 3000"));
